b204b37bb265a1fa8353aee6992201600c4511eb
liberta09 <kaanokan258@gmail.com>
2026-09-22 13:14:15 +0300

    Set plugin status to 3 (Working) so CloudStream recognizes the plugin

--- HDFilmCehennemi/build.gradle.kts
+++ HDFilmCehennemi/build.gradle.kts
@@ -3,7 +3,7 @@ cloudstream {
     authors = listOf("liberta09")
     language = "tr"
     description = "Türkiye'nin en hızlı hd film izleme sitesi"
-    status = 1
+    status = 3
     tvTypes = listOf("Movie", "TvSeries")
     iconUrl = "https://www.google.com/s2/favicons?domain=hdfilmcehennemi.nl&sz=%size%"
     setRepo("liberta09/liberta09")
