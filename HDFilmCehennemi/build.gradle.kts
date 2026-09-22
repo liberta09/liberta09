4962572162757d75d5e06b2b3a820c138d2e666a
liberta09 <kaanokan258@gmail.com>
2026-09-22 12:58:23 +0300

    Set plugin repo property for valid download URL in plugins.json

--- HDFilmCehennemi/build.gradle.kts
+++ HDFilmCehennemi/build.gradle.kts
@@ -6,4 +6,5 @@ cloudstream {
     status = 1
     tvTypes = listOf("Movie", "TvSeries")
     iconUrl = "https://www.google.com/s2/favicons?domain=hdfilmcehennemi.nl&sz=%size%"
+    setRepo("liberta09/liberta09")
 }
